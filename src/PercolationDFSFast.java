53e0bad607c009ef95ac1324a3193754c3397012
Ritik Goyal <goyalritik2000@gmail.com>
2018-12-02 22:32:49 -0500

    Finished DFS Fast

---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,40 @@
+
+public class PercolationDFSFast extends PercolationDFS{
+
+    /**
+     * Initialize myGrid of size (size x size)
+     * @param size - size of myGrid
+     */
+    public PercolationDFSFast(int size){
+        super(size);
+    }
+
+    @Override
+    /**
+     * Determine of cell should be marked as full and if so, call dfs(row, col)
+     * @param row - row of cell
+     * @param col - column of cell
+     */
+    protected void updateOnOpen(int row, int col) {
+        //If cell is in top row it should be marked as full
+        if (row == 0){
+            dfs(row,col);
+            return;
+        }
+        //Check if any of its four neighbors are full; if at least one of them is, then cell should be marked as full
+        else {
+            int [] rowDelta = {-1, 1, 0, 0};
+            int [] colDelta = {0, 0, -1, 1};
+            for (int i = 0; i < rowDelta.length; i++){
+                int rowtemp = row + rowDelta[i];
+                int coltemp = col + colDelta[i];
+                if (inBounds(rowtemp, coltemp)){
+                    if (isFull(rowtemp, coltemp)){
+                        dfs(row, col);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
